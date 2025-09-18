dd59b09391980ee883d05196123c3040d5fd0c14
Matias Lorda <mlorda@frba.utn.edu.ar>
2025-09-18 09:50:50 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,42 @@
-// inicio del codigo 
+// inicio del codigo (18/09/25)
 
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
+
+struct Productos {
+
+  string sku;
+  string descripcion;
+  float costo_fijo;
+
+}
+
+struct Reparaciones {
+
+  string cliente;
+  int tipo_de_producto;
+  string sku;
+  float costo_fijo;
+  float presupuestado;
+
+}
+
+
+void busqueda_lineal () {
+
+}
+
+void ordenamiento_ascendente() {
+
+}
+
+
 int main() {
 
-  cout << "Hola Mundo << endl;
+  cout << "Hola Mundo" << endl;
 
   return 0;
 }
