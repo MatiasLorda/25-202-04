81aadab5c4852ef7f294407b69eda07b77eb29e3
Matias Lorda <mlorda@frba.utn.edu.ar>
2025-09-22 12:16:29 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,6 +13,13 @@ struct Productos {
   string descripcion;
   float costo_fijo;
 
+  Productos *siguiente = nullptr;
+
+};
+
+void insertarProductos (Productos *&lista) {
+
+
 }
 
 struct Reparaciones {
@@ -22,6 +29,13 @@ struct Reparaciones {
   string sku;
   float costo_fijo;
   float presupuestado;
+
+  Reparaciones *siguiente = nullptr;
+
+};
+
+void insertarReparaciones (Reparaciones *&lista) {
+
 
 }
 
@@ -44,17 +58,21 @@ void lecturaProductos() {
 
   archivoProductos.open("productos.bin");
 
-  if (archivoProductos.fail()) {
-    cout << "No se pudo abrir el archivo de producos" << endl;
+  if (!archivoProductos) {
+    cout << "No se pudo abrir correctamente el archivo de Productos" << endl;
     exit(1);
   }
-  
-  while (!archivoProductos.eof()) {
-    getline(archivoProductos,texto);
-    cout << texto << endl;
-  }
 
+ /* si quisiera escribir el archivo que leí: 
+  
+ getline (archivoProductos, texto);
+
+ cout << texto << endl;
+
+ */
+  
  archivoProductos.close();
+  
 }
 
 //Lectura de archivo de reparaciones (18/09/25)
@@ -66,33 +84,80 @@ void lecturaReparaciones() {
 
   archivoReparaciones.open("reparaciones.bin");
 
-  if archivoReparaciones.fail(){
-    cout << "No se pudo abrir el archivo de reparaciones" << endl;
+  if(!archivoReparaciones) {
+    cout << "No se pudo abrir correctamente el archivo de Reparaciones" << endl;
     exit(1);
   }
+  
+  /* si quisiera escribir el archivo que leí: 
 
-  while (!archivoReparaciones.eof()) {
-    getline(archivoReparaciones,texto);
-    cout << texto << endl;
-  }
+  getline(archivoReparaciones, texto);
+
+  cout << texto << endl;
+
+  */
 
   archivoReparaciones.close();
+
 }
 
-//Descripcion del tipo de producto
-string descripcionTipo(int tipo) {
-    switch (tipo) {
-        case 0: return "Electrónico";
-        case 1: return "Mecánico";
-        case 2: return "Mecatrónico";
-        default: return "Desconocido";
+// Función Descripción del Tipo de Producto (21/09/25)
+
+void descripcionTipo() {
+
+    Reparaciones t;
+
+    switch (t.tipo_producto) {
+
+        case 0:
+            cout << "Electrónico";
+            break;
+        case 1:
+            cout << "Mecánico";
+            break;
+        case 2:
+            cout << "Mecatrónico";
+            break;
+
+        default: cout << "Desconocido";
+
+        }
     }
-}
 
 int main() {
 
   lecturaProductos();
   lecturaReparaciones();
+
+  descripcionTipo();
+
+  // Carga de Clientes y Comienzo del Main (22/09/25)
+
+  const string cliente1 = "Nyota Uhura";
+  const string cliente2 = "Don Ramón";
+  const string cliente3 = "Taller el Trio";
+
+  const int dim {7};
+
+  string datos[dim] = {"Cliente", "Tipo Producto", "SKU", "Producto", "Costo Fijo", "Costo Directo", "Presupuestado"};
+
+  while (cin) {
+
+      Reparaciones c;
+
+      cout << "Ingrese cliente a consultar: ";
+      cin >> c.cliente;
+
+      while (c.cliente.length() > 15) {
+
+          cout << "El largo del nombre no puede ser mas de 15 caracteres!" << endl;
+          cout << "Ingrese nuevamente el cliente a consultar: ";
+          cin >> c.cliente;
+      }
+
+  }
+    
+  
 
   return 0;
 }
